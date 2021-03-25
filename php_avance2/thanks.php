27fe5d92e7c2cce26a7e96a74dc3fb1d6c82ffea
HEMMER <79737617+HEMMER-FX@users.noreply.github.com>
2021-03-25 13:19:47 +0100

    Update thanks.php

--- php_avance2/thanks.php
+++ php_avance2/thanks.php
@@ -1,21 +1,24 @@
 <?php
 
-$email = filter_var($_POST['user_email'], FILTER_VALIDATE_EMAIL);
 
 if(isset($_POST['submit_button']))
 {
-    if(!empty($_POST['user_lastname']) AND !empty($_POST['user_name']) AND !empty($_POST['object']) AND !empty($_POST['user_email']) || filter_var($_POST['user_email'], FILTER_VALIDATE_EMAIL) AND
+    if(!empty($_POST['user_lastname']) AND !empty($_POST['user_name']) AND !empty($_POST['object']) AND !empty($_POST['user_email']) AND
     !empty($_POST['user_message']) AND !empty($_POST['tel']))
     {
-        echo 'Merci ' . $_POST['user_lastname'] . ' ' . $_POST['user_name'] . ' de nous avoir contacté à propos de ' . $_POST['object'] . '. <br>';
-        echo ' Un de nos conseiller vous contactera soit à l’adresse ' . $_POST['user_email'] . ' ou par téléphone au ' . $_POST['tel'] . ' dans les plus brefs délais pour traiter votre demande : <br><br> ';
-        echo $_POST['user_message'];
-    } else
+        if (isset($_POST['user_email']) && filter_var($_POST['user_email'], FILTER_VALIDATE_EMAIL))
+        {         
+            echo 'Merci ' . $_POST['user_lastname'] . ' ' . $_POST['user_name'] . ' de nous avoir contacté à propos de ' . $_POST['object'] . '. <br>';
+            echo ' Un de nos conseiller vous contactera soit à l’adresse ' . $_POST['user_email'] . ' ou par téléphone au ' . $_POST['tel'] . ' dans les plus brefs délais pour traiter votre demande : <br><br> ';
+            echo $_POST['user_message'];
+        }
+        else
+        {
+            echo "votre email n'est pas valide, merci de faire ne nécessaire";
+        }
+    }
+    else
     {
-        echo "tout les champs ne sont pas remplis";
+        echo "manque des champs";
     }
 }
-
-
-
-?>
